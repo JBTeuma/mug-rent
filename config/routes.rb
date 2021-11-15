766f48d1430ebf824e2595537b8f5c9317962382
JeanBatiste Teuma <teuma.jeanbaptiste@gmail.com>
2021-11-15 17:20:21 +0100

    first seedand routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :mugs, except: %i[destroy update edit] do
+    resources :bookings, only: %i[new create]
+  end
+
+  resources :bookings, only: [] do
+    member do
+      patch 'validates'
+      patch 'deny'
+    end
+  end
+
+  resource :dashboard, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+p '-------Creating 2 users---------'
+john = User.new(username:'John', email: 'john@beetles.com')
+john.password = '12345678'
+john.save!
+ringo = User.new(username:'Ringo', email: 'ringo@beetles.com')
+ringo.password = '12345678'
+ringo.save!
+p '-------Creating 2 mugs----------'
+licorn = Mug.create!(color:'pink', user: john)
+paddington = Mug.create!(color:'red', user: john)
+p '-------Creating 1 booking-------'
+booking = Booking.create!(start_date: Date.today, end_date: Date.today + 5, user: ringo, mug: licorn)
