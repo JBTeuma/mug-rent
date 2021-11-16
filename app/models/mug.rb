74a3e8932febeaaec92434bf489621d27dd4fab7
Louis Quenault <louis.quenault@laposte.net>
2021-11-16 12:59:06 +0100

    Set validates on all models

--- app/models/mug.rb
+++ app/models/mug.rb
@@ -3,4 +3,10 @@ class Mug < ApplicationRecord
   has_many :users, through: :bookings
 
   belongs_to :user
+  validates :description, presence: true
+  validates :color, presence: true
+  validates :size, presence: true
+  validates :price_by_day, presence: true
+  validates :price_by_day, numericality: { only_integer: true }
+  validates :size, inclusion: { in: %w[s m l], message: "%{value} is not a valid size" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :adress, presence: true
+  validates :username, presence: true
+  validates :email, presence: true
 end
