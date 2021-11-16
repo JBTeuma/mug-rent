4110f1965ab5aadd28e8693051ab8bbeeb7e24de
Louis Quenault <louis.quenault@laposte.net>
2021-11-16 15:09:36 +0100

    Setup default value for bookings:status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_151022) do
+ActiveRecord::Schema.define(version: 2021_11_16_135824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_151022) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.integer "cost"
-    t.string "status"
+    t.string "status", default: "pending"
     t.bigint "mug_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
