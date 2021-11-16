d2efd56bdcffc0e0277981cabd63da4232c356d3
Louis Quenault <louis.quenault@laposte.net>
2021-11-16 16:55:10 +0100

    Set method #new and form attached with

--- app/controllers/mugs_controller.rb
+++ app/controllers/mugs_controller.rb
@@ -7,4 +7,9 @@ class MugsController < ApplicationController
   def create
   end
 
+  private
+
+  def mug_params
+    params.require(:mugs).permit(:color, :description, :size, :price_by_day, photo: [])
+  end
 end
