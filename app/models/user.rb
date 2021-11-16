6830ee91f49870446e4d8afe3bc1b57ba78ac5fb
Louis Quenault <louis.quenault@laposte.net>
2021-11-16 13:13:25 +0100

    Error on adress

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :adress, presence: true
+  validates :address, presence: true
   validates :username, presence: true
   validates :email, presence: true
 end
