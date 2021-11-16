46f643f7d31bfa3857ab047d01730560540b6e11
Louis Quenault <louis.quenault@laposte.net>
2021-11-16 17:53:47 +0100

    Set mugs#new

--- app/controllers/mugs_controller.rb
+++ app/controllers/mugs_controller.rb
@@ -1,15 +1,24 @@
 class MugsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
 
   def new
     @mug = Mug.new
   end
 
   def create
+    @mug = Mug.new(mug_params)
+    @mug.user = current_user
+    if @mug.save
+      flash[:notice] = "Mug was successfully created"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def mug_params
-    params.require(:mugs).permit(:color, :description, :size, :price_by_day, photo: [])
+    params.require(:mug).permit(:color, :description, :size, :price_by_day, :photo)
   end
 end
