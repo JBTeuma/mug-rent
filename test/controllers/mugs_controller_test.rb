cacae0549b94706f120489492cff210b051396e2
JeanBatiste Teuma <teuma.jeanbaptiste@gmail.com>
2021-11-16 12:05:56 +0100

    create cMugs controller and index method/view

---
+++ test/controllers/mugs_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class MugsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get mugs_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get mugs_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get mugs_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get mugs_index_url
+    assert_response :success
+  end
+
+end
