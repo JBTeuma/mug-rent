931db80374f797a46028638194a2e2a99caed4aa
Louis Quenault <louis.quenault@laposte.net>
2021-11-17 13:30:15 +0100

    Set booking

---
+++ app/javascript/components/cost_calcul.js
@@ -0,0 +1,32 @@
+const calcul = () => {
+  const costElement = document.querySelector(".cost")
+  const startDateInput = document.getElementById("booking_start_date")
+  const endDateInput = document.getElementById("booking_end_date")
+
+  const elementPriceByDay = document.getElementById("new_booking")
+  const priceByDay = parseInt(elementPriceByDay.dataset.pricebyday, 10)
+
+  const startDate = new Date(startDateInput.value)
+  const endDate = new Date(endDateInput.value)
+  const cost = (((endDate - startDate) / 24 / 60 / 60 / 1000) + 1) * priceByDay
+
+  if (cost >= priceByDay) {
+    costElement.innerText = `${cost} €`
+    document.querySelector(".submit").disabled = false;
+  } else {
+    window.alert("La date de début doit être inférieur à la date de fin de location")
+    document.querySelector(".submit").disabled = true;
+  }
+}
+
+const costCalcul = () => {
+  const element = document.getElementById("new_booking")
+  if (!element) return
+
+  const startDate = document.getElementById("booking_start_date")
+  const endDate = document.getElementById("booking_end_date")
+  startDate.addEventListener("change", calcul)
+  endDate.addEventListener("change", calcul)
+};
+
+export {  costCalcul }
